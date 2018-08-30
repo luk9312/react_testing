6197081b64c623a99ca4d6a2434afdd2903d2135
dustin.luk <kingsan212@gmail.com>
2018-08-30 15:13:35 +0800

    redux+todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { v4 } from 'node-uuid';
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import * as api from '../data';
 import { getIsFetching } from '../reducers';
 
@@ -18,7 +19,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
       dispatch({
         type: 'FETCH_TODOS_SUCCESS',
         filter,
-        response
+        response: normalize(response, schema.arrayOfTodos)
       });
     },
     error => {
@@ -31,13 +32,18 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
   );
 };
 
-export const addTodo = (text) =>  ({
-    type: 'ADD_TODO',
-    id: v4(),
-    text
-});
+export const addTodo = (text) => (dispatch) =>
+  api.addTodo(text).then(response => {
+    dispatch({
+      type: 'ADD_TODO_SUCCESS',
+      response: normalize(response, schema.todo)
+    });
+  });
 
-export const toggleTodo = (id) => ({
-  type: 'TOGGLE_TODO',
-  id
-});
+export const toggleTodo = (id) => (dispatch) =>
+  api.toggleTodo(id).then(response => {
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, schema.todo)
+    })
+  })
